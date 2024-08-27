074d64174a0a5f29564af323f5768ef16df62180
Umar Qureshi <161929656+Umerqureshi786@users.noreply.github.com>
2024-08-27 07:31:51 -0700

    Flutter work

---
+++ demo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Kuchbhi());
+
+}
+
+class Kuchbhi extends StatelessWidget{
+Widget build(BuildContext context){
+  return MaterialApp(home: Text("Hello world"), title: "practice",);
+}
+}
+
+class Rehan extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(home: Text("Hello Rehan"), title: "My appp ",);
+
+  }
+}
+
+class Umerahsan extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(home: Text("My name is umarahsan"), title: "umar app",);
+  }
+}
